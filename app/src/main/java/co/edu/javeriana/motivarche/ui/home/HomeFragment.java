c9dcce3694008414a8a862305012357209f8fd74
DanielaBeltranSaavedra <d_beltran@javeriana.edu.co>
2021-09-04 16:09:35 -0500

    add Home, profile, tutorial fragment

--- app/src/main/java/co/edu/javeriana/motivarche/ui/home/HomeFragment.java
+++ app/src/main/java/co/edu/javeriana/motivarche/ui/home/HomeFragment.java
@@ -1,9 +1,14 @@
 package co.edu.javeriana.motivarche.ui.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +20,29 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View homeView = inflater.inflate(R.layout.fragment_home, container, false);
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        MediaController mc= new MediaController(getActivity());
+        VideoView view = (VideoView)homeView.findViewById(R.id.videoView7);
+        String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.video_prueba;
+        view.setVideoURI(Uri.parse(path));
+        view.setMediaController(mc);
+        alert.setMessage("Aceptar las Condiciones del servicio y la Política de privacidad de MotivARCHE")
+                .setCancelable(false)
+                .setPositiveButton("Acepto", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        try {
+                            finalize();
+                            view.start();
+                        } catch (Throwable throwable) {
+                            throwable.printStackTrace();
+                        }
+                    }
+                });
+        AlertDialog title = alert.create();
+        title.setTitle("Bienvenido");
+        title.show();
+
         return homeView;
     }
 
