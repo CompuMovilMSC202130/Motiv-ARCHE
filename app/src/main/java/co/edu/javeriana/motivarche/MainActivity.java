e0b8015cdab7f1c98324a663b65a9f60981ee045
Juan Camilo González Vargas <juan.gonzalez@kibernum.com.co>
2021-09-03 19:10:04 -0500

    se agrega opcion para ver interaccion de ingresar a la app

--- app/src/main/java/co/edu/javeriana/motivarche/MainActivity.java
+++ app/src/main/java/co/edu/javeriana/motivarche/MainActivity.java
@@ -2,13 +2,26 @@ package co.edu.javeriana.motivarche;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnIniciarSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),PrincipalMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
