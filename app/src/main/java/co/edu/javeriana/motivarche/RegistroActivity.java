fd4a12cf7cc702130bde3988782fc296bfa32243
Frank94co <frank9412_co@hotmail.com>
2021-09-03 22:26:14 -0500

    Pantalla del registro construida y hechos dos intents que conectan ambas pantallas.

--- app/src/main/java/co/edu/javeriana/motivarche/RegistroActivity.java
+++ app/src/main/java/co/edu/javeriana/motivarche/RegistroActivity.java
@@ -5,20 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tv;
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro);
     }
 
-    public void irAlRegistro(View v){
-        Intent intent= new Intent(getApplicationContext(), RegistroActivity.class);
+    public void irAPantallaPrincipal(View v){
+        Intent intent= new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
-
 }
