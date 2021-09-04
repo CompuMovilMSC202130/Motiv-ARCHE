33e4c42e59bf0d3c226aa6929727147a5a81487e
Juan Camilo González Vargas <juan.gonzalez@kibernum.com.co>
2021-09-04 12:03:35 -0500

    se agrega la acción de cerrar sesión

--- app/src/main/java/co/edu/javeriana/motivarche/PrincipalMenu.java
+++ app/src/main/java/co/edu/javeriana/motivarche/PrincipalMenu.java
@@ -1,9 +1,11 @@
 package co.edu.javeriana.motivarche;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -31,6 +33,7 @@ public class PrincipalMenu extends AppCompatActivity {
         setSupportActionBar(findViewById(R.id.toolbar));
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        addClickOptionCloseSession();
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_scanner, R.id.nav_museum,R.id.nav_profile,R.id.nav_tutorial,R.id.nav_close_session)
                 .setOpenableLayout(drawer)
@@ -38,6 +41,18 @@ public class PrincipalMenu extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void addClickOptionCloseSession() {
+        MenuItem item = navigationView.getMenu().findItem(R.id.nav_close_session);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent i = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+                return false;
+            }
+        });
     }
 
     @Override
